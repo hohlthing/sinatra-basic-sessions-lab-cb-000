1d274d2b7c0b69bb45ccb401fd7c5466731ed6ee
michael hohl <mhohl@rocketmail.com>
2018-05-18 01:19:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, 'flatiron'
+  end
+
+  get '/' go
+    erb: index
+  end
+  
 end
