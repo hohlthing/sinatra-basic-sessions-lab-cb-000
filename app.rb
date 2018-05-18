e33e8a0b22040d41052290faaa568d33ddbc3d43
michael hohl <mhohl@rocketmail.com>
2018-05-18 01:20:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,11 +4,17 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, 'flatiron'
+    set :session_secret, 'goingbacktocali'
   end
 
   get '/' go
     erb: index
   end
-  
+
+  post '/checkout' do 
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+
 end
